0816ec4414431e36b0036e2ed2bc55bd412a8b4a
Makoto <2762006003@qq.com>
2025-10-09 15:58:35 +0800

    hot100第15题完成

---
+++ problem2/src/problem15.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem15 {
+    public static void main(String[] args) {
+        Solution15 solution15 = new Solution15();
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//        int[][] matrix = {
+//                {1, 2, 3, 4},
+//                {5, 6, 7, 8},
+//                {9, 10, 11, 12}
+//        };
+        int[][] matrix = {
+                {1, 2, 3, 4}
+        };
+        List<Integer> result = solution15.spiralOrder(matrix);
+        System.out.println(result);
+    }
+}
+
+class Solution15 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (matrix.length == 0) {
+            return res;
+        }
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            // 从右到左（需要检查是否还有行）
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // 从下到上（需要检查是否还有列）
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
