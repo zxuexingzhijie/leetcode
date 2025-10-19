ba3190d53f931f00d177b6014bb41b4a8db7dd4f
Makoto <2762006003@qq.com>
2025-10-19 21:55:11 +0800

    hot100第45题完成

---
+++ problem2/src/problem45.java
@@ -0,0 +1,13 @@
+public class problem45 {
+    public static void main(String[] args) {
+        Solution45 solution45 = new Solution45();
+
+    }
+}
+
+
+class Solution45 {
+    public int pathSum(TreeNode root, int targetSum) {
+        return 0;
+    }
+}
