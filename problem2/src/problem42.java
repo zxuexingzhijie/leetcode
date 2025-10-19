d3b4f507ed1270f4920cc40137a9c6596880f052
Makoto <2762006003@qq.com>
2025-10-19 17:19:38 +0800

    hot100第42题完成

---
+++ problem2/src/problem42.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class problem42 {
+    public static void main(String[] args) {
+        Solution42 solution42 = new Solution42();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+        List<Integer> res = solution42.rightSideView(root);
+        res.stream().forEach(System.out::println);
+    }
+}
+
+class Solution42 {
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        Queue<TreeNode> treeNodes = new LinkedList<>();
+        treeNodes.offer(root);
+
+        while (!treeNodes.isEmpty()){
+            int size = treeNodes.size();
+            for (int i = 0; i < size; i++){
+                TreeNode node = treeNodes.poll();
+                if (i == size - 1){
+                    res.add(node.val);
+                }
+                if (node.left != null){treeNodes.offer(node.left);}
+                if (node.right != null){treeNodes.offer(node.right);}
+            }
+        }
+        return res;
+    }
+}
