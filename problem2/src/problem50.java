6bf84ef0c57ba37f7ca6fcc0d5f66d42cd5446b0
Makoto <2762006003@qq.com>
2025-10-20 22:55:28 +0800

    hot100第50题没有完成

---
+++ problem2/src/problem50.java
@@ -0,0 +1,33 @@
+public class problem50 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Trie {
+
+    public Trie() {
+
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+        return false;
+    }
+
+    public boolean startsWith(String prefix) {
+        return false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
