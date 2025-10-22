7a4de1c1113acb0fc84b39309358f8ef6b5eebb0
Makoto <2762006003@qq.com>
2025-10-22 12:16:19 +0800

    hot100第63题完成

---
+++ problem2/src/problem63.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class problem63 {
+    public static void main(String[] args) {
+        Solution63 solution63 = new Solution63();
+        boolean valid = solution63.isValid("()[]{}");
+        System.out.println(valid);
+    }
+}
+
+
+class Solution63 {
+    public boolean isValid(String s) {
+        Stack<Character> res = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' ||  s.charAt(i) == '[' || s.charAt(i) == '{') {
+                res.push(s.charAt(i));
+            }else if (res.isEmpty()){
+                return false;
+            }
+            else if (s.charAt(i) == ')' && res.pop() != '(') {
+                return false;
+            }else if (s.charAt(i) == ']' && res.pop() != '[') {
+                return false;
+            }else if (s.charAt(i) == '}' && res.pop() != '{') {
+                return false;
+            }
+        }
+        return res.isEmpty();
+    }
+}
