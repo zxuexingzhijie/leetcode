9c63f69c09f24d6f53af23f05e24a7de9c3a82c2
Makoto <2762006003@qq.com>
2025-10-22 13:29:08 +0800

    hot100第65题未完成

---
+++ problem2/src/problem65.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class problem65 {
+    public static void main(String[] args) {
+        Solution65 solution65 = new Solution65();
+        String s = new String("3[a]2[bc]");
+        String res = solution65.decodeString(s);
+        System.out.println(res);
+    }
+}
+
+class Solution65 {
+    public String decodeString(String s) {
+        StringBuffer res = new StringBuffer();
+        Stack<Character> characters = new Stack<>();
+
+        return null;
+
+    }
+}
