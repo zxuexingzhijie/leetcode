9ad86bb85aaa36e1803af15ac84f7ab9218313af
Makoto <2762006003@qq.com>
2025-10-23 10:59:50 +0800

    hot100第73题完成

---
+++ problem2/src/problem73.java
@@ -0,0 +1,19 @@
+public class problem73 {
+    public static void main(String[] args) {
+        Solution73 solution73 = new Solution73();
+        int n = 3;
+        int res = solution73.climbStairs(n);
+        System.out.println(res);
+    }
+}
+
+class Solution73 {
+    public int climbStairs(int n) {
+        int[] f = new int[n + 1];
+        f[0] = f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n];
+    }
+}
