3131d5445cd9f84df593d8d411938fa0213cd313
Makoto <2762006003@qq.com>
2025-10-23 16:20:37 +0800

    hot100第77题未完成

---
+++ problem2/src/problem77.java
@@ -0,0 +1,17 @@
+public class problem77 {
+    public static void main(String[] args) {
+        Solution77 solution77 = new Solution77();
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        int i = solution77.coinChange(coins, amount);
+        System.out.println(i);
+    }
+}
+
+
+class Solution77 {
+    public int coinChange(int[] coins, int amount) {
+        return 0;
+
+    }
+}
