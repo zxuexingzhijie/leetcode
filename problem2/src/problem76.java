93d0a202be658ba4331f51aaca3c584891fb8ae2
Makoto <2762006003@qq.com>
2025-10-23 15:48:10 +0800

    hot100第76题未完成

---
+++ problem2/src/problem76.java
@@ -0,0 +1,15 @@
+public class problem76 {
+    public static void main(String[] args) {
+        Solution76 solution76 = new Solution76();
+        int n = 12;
+        int i = solution76.numSquares(n);
+        System.out.println(i);
+    }
+}
+
+
+class Solution76 {
+    public int numSquares(int n) {
+
+    }
+}
