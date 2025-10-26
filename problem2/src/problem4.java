5d6b4eae22392d8c177309272a0b7c0e6127a6c9
Makoto <2762006003@qq.com>
2025-10-26 14:49:45 +0800

    hot100更新第4题

--- problem2/src/problem4.java
+++ problem2/src/problem4.java
@@ -15,14 +15,14 @@ public class problem4 {
 
 class Solution4 {
     public void moveZeroes(int[] nums) {
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                if (i != j) {
-                    nums[i] = 0;
-                }
-                j++;
+        int left = 0; // 指向下一个要放非零元素的位置
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                // 交换非零元素到前面
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
             }
         }
     }
