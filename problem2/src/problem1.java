61283218116505d29eab9e17562ce7476f6d851c
Makoto <2762006003@qq.com>
2025-10-26 12:02:28 +0800

    hot100更新第一题

--- problem2/src/problem1.java
+++ problem2/src/problem1.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 // LeetCode题目链接: https://leetcode.cn/problems/two-sum/
 public class problem1 {
     public static void main(String[] args) {
@@ -12,15 +14,14 @@ public class problem1 {
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] list = new int[2];
+        HashMap<Integer, Integer> res = new HashMap<>();
         for(int i = 0;i<nums.length;i++){
-            for (int j = i+1;j<nums.length;j++){
-                if (nums[i]+nums[j] == target){
-                    list[0] = i;
-                    list[1] = j;
-                }
+            if (res.containsKey(target-nums[i])){
+                return new int[]{res.get(target-nums[i]),i};
             }
+            res.put(nums[i],i);
         }
-        return list;
+        return null;
+
     }
 }
