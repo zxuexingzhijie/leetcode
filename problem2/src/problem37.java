353889f38034a7f59255e8e038cb9e80126a7acf
Makoto <2762006003@qq.com>
2025-10-27 18:43:30 +0800

    hot100更新第37题

--- problem2/src/problem37.java
+++ problem2/src/problem37.java
@@ -1,5 +1,7 @@
 // LeetCode题目链接: https://leetcode.cn/problems/binary-tree-level-order-traversal/
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class problem37 {
@@ -19,7 +21,34 @@ public class problem37 {
 
 class Solution37 {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        return new ArrayList<List<Integer>>();
+        ArrayList<List<Integer>> res = new ArrayList<>();
+
+        Deque<TreeNode> nodes = new ArrayDeque<>();
+        nodes.offer(root);
+
+        while(!nodes.isEmpty()) {
+            int size = nodes.size();
+            ArrayList<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = nodes.poll();
+                if (node != null) {
+                    list.add(node.val);
+                }else  {
+                    list.add(null);
+                }
+
+                if (node.left != null) {
+                    nodes.offer(node.left);
+                }
+                if (node.right != null) {
+                    nodes.offer(node.right);
+                }
+            }
+            res.add(list);
+        }
+
+        return res;
 
     }
 }
