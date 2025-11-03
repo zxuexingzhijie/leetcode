6a9619fd432204bdc6d153e120eb3ecabc6e06ca
Makoto <2762006003@qq.com>
2025-11-03 21:46:39 +0800

    hot100更新第78题

--- problem2/src/problem78.java
+++ problem2/src/problem78.java
@@ -1,47 +1,35 @@
-//https://leetcode.cn/problems/trapping-rain-water/?
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class problem78 {
     public static void main(String[] args) {
         Solution78 solution78 = new Solution78();
-        int[] trap = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int res = solution78.trap(trap);
-        System.out.println(res);
+        String s = "leetcode";
+        ArrayList<String> wordDict = new ArrayList<>();
+        wordDict.add("leet");
+        wordDict.add("code");
+        boolean option = solution78.wordBreak(s, wordDict);
+        System.out.println(option);
     }
 }
-
 
 class Solution78 {
-    public int trap(int[] height) {
-        if (height == null || height.length == 0) {
-            return 0;
-        }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
 
-        int left = 0;                     // 左指针
-        int right = height.length - 1;    // 右指针
-        int leftMax = 0;                  // 左边最高柱子
-        int rightMax = 0;                 // 右边最高柱子
-        int sum = 0;                      // 总水量
-
-        while (left < right) {
-            if (height[left] < height[right]) {
-                // 左边较矮，水量取决于左边最高
-                if (height[left] >= leftMax) {
-                    leftMax = height[left]; // 更新左边最高柱子
-                } else {
-                    sum += leftMax - height[left]; // 当前能接的水
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
                 }
-                left++; // 左指针右移
-            } else {
-                // 右边较矮，水量取决于右边最高
-                if (height[right] >= rightMax) {
-                    rightMax = height[right]; // 更新右边最高柱子
-                } else {
-                    sum += rightMax - height[right]; // 当前能接的水
-                }
-                right--; // 右指针左移
             }
         }
-
-        return sum;
+        return dp[n];
     }
 }
-
